9201d9e032fef7a66da7e003867589f7de436f53
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2024-07-24 08:53:08 +0530

    Update check_images.py

--- Pre-trained-Image-Classifier-to-Identify-Dog-Breeds-master/check_images.py
+++ Pre-trained-Image-Classifier-to-Identify-Dog-Breeds-master/check_images.py
@@ -3,8 +3,8 @@
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: Annika Lisa Lang
-# DATE CREATED: 15.06.2020                                 
+# PROGRAMMER: Madhu Shree Aravindan
+# DATE CREATED: 24.07.2024                                
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
